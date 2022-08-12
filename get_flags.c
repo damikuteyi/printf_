57cbc3edb089cb280baccd8f3dd4e69c8e713037
= <=>
2022-08-11 21:54:29 -0700

    updated files

--- get_flags.c
+++ get_flags.c
@@ -1,27 +1,19 @@
 #include "main.h"
-
-
-
 /**
+ * get_flags - Calculates active flags
  *
- * * get_flags - Calculates active flags
+ * @format: Formatted string in which to print the arguments
  *
- * * @format: Formatted string in which to print the arguments
+ * @i: take a parameter.
  *
- * * @i: take a parameter.
+ * Return: Flags:
  *
- * * Return: Flags:
- *
- * */
-
-
+ */
 
 int get_flags(const char *format, int *i)
 
 {
-
 	/* - + 0 # ' ' */
-
 	/* 1 2 4 8  16 */
 
 	int j, curr_i;
@@ -32,12 +24,9 @@ int get_flags(const char *format, int *i)
 
 	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
-
-
 	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 
 	{
-
 		for (j = 0; FLAGS_CH[j] != '\0'; j++)
 
 			if (format[curr_i] == FLAGS_CH[j])
@@ -47,10 +36,7 @@ int get_flags(const char *format, int *i)
 				flags |= FLAGS_ARR[j];
 
 				break;
-
 			}
-
-
 
 		if (FLAGS_CH[j] == 0)
 
@@ -58,12 +44,7 @@ int get_flags(const char *format, int *i)
 
 	}
 
-
-
 	*i = curr_i - 1;
 
-
-
 	return (flags);
-
 }
