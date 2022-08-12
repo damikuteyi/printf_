b68d70b5e56c29992bc069ffadc2ce333e71ec01
damikuteyi <kuteyidamilola@gmail.com>
2022-08-12 02:09:53 -0700

    update to the files

--- functions.c
+++ functions.c
@@ -116,7 +116,6 @@ int print_int(va_list types, char buffer[],
 
 		int flags, int width, int precision, int size)
 {
-
 	int i = BUFF_SIZE - 2;
 	int is_negative = 0;
 	long int n = va_arg(types, long int);
